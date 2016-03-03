6c8adf446109ca201e671547d8e3c347d2f0753b
Ziyi <kwokjona@gmail.com>
2016-03-03 16:35:50 +0200

    Space Ttckiting

---
+++ clients/javascript/ai/myai/ai.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var _ = require("lodash");
+var position = require("../../position.js");
+
+// Change botNames and teamName to your choice.
+var botNames = [
+  "Zero",
+  "One",
+  "Two"
+];
+
+module.exports = function Ai() {
+  function makeDecisions(roundId, events, bots, config) {
+    bots.forEach(function(bot) {
+      var ps = position.neighbours(position.make(bot.x, bot.y), config.move);
+      var pos = ps[randInt(0, ps.length - 1)];
+      var rdPos = position.make(randInt(-10,10),randInt(-10,10));
+      if(roundId%4 === 0){
+        bot.move(pos.x, pos.y);
+      }else{
+        bot.radar(rdPos.x,rdPos.y);
+      }
+    });
+    _.each(events, function(event) {
+      if (event.event === "noaction") {
+        console.log("Bot did not respond in required time", event.data);
+      }
+    });
+  }
+
+  function randInt(min, max) {
+    var range = max - min;
+    var rand = Math.floor(Math.random() * (range + 1));
+    return min + rand;
+  }
+
+  return {
+    // The AI must return these three attributes
+    botNames: botNames,
+    makeDecisions: makeDecisions
+  };
+};
---
+++ clients/python/tyckiting_client/ai/your_ai.py
@@ -0,0 +1,36 @@
+import random
+
+from tyckiting_client.ai import base
+from tyckiting_client import actions
+
+
+class Ai(base.BaseAi):
+    """
+    Dummy bot that moves randomly around the board.
+    """
+    def move(self, bots, events):
+        """
+        Move the bot to a random legal positon.
+
+        Args:
+            bots: List of bot states for own team
+            events: List of events form previous round
+
+        Returns:
+            List of actions to perform this round.
+        """
+        response = []
+        for bot in bots:
+            if not bot.alive:
+                continue
+            event = events[bot.bot_id];
+            if event['event'] == "see":
+                response.append(actions.Cannon(bot_id=bot.bot_id,
+                                         x=event.pos.x,
+                                         y=event.pos.y))
+            else:
+                move_pos = random.choice(list(self.get_valid_moves(bot)))
+                response.append(actions.Move(bot_id=bot.bot_id,
+                                         x=move_pos.x,
+                                         y=move_pos.y))
+        return response
