907e04b1ee12e0d9831a4cd92d1adf4fcab59597
Frank Müller <frmimue@gmail.com>
2016-03-14 18:48:34 +0200

    fixed

--- run_mm.bat
+++ run_mm.bat
@@ -1,4 +1,4 @@
 start node .\server\start-server.js
 start C:\Python27\python.exe .\clients\python\cli.py --ai himanshuv2
-start node .\clients\javascript\cli.js mastermind
+start node .\clients\javascript\cli.js --ai mastermind
 start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://localhost:3000"
